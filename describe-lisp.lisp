bc63fb0449ef452c788097ebad6563695040f173
Michaël Le Barbier <michael.le-barbier@melusina.org>
2023-09-13 06:35:28 +0200

    Import GitHub Action to Setup Common Lisp

---
+++ describe-lisp.lisp
@@ -0,0 +1,73 @@
+;;;; describe-lisp.lisp — Setup Common Lisp
+
+;;;; Melusina Actions (https://github.com/melusina-org/setup-common-lisp)
+;;;; This file is part of Melusina Actions.
+;;;;
+;;;; Copyright © 2023 Michaël Le Barbier
+;;;; All rights reserved.
+
+;;;; This file must be used under the terms of the MIT License.
+;;;; This source file is licensed as described in the file LICENSE, which
+;;;; you should have received as part of this distribution. The terms
+;;;; are also available at https://opensource.org/licenses/MIT
+
+(require '#:asdf)
+(require '#:uiop)
+
+(defpackage #:org.melusina.lisp-action/describe-lisp
+  (:use #:common-lisp))
+
+(in-package #:org.melusina.lisp-action/describe-lisp)
+
+(defparameter *init-filename*
+  '((allegro . ".clinit.cl")
+    (abcl . ".abclrc")
+    (ccl .
+     #+windows
+     "ccl-init.lisp"
+     #-windows
+     ".ccl-init.lisp")
+    (clasp . ".clasprc")
+    (clisp . ".clisprc.lisp")
+    (ecl . ".eclrc")
+    (mkcl . ".mkclrc")
+    (lispworks . ".lispworks")
+    (sbcl . ".sbclrc")
+    (cmucl . ".cmucl-init.lisp")
+    (scl . ".scl-init.lisp")))
+
+(defun init-filename ()
+  (merge-pathnames
+   (cdr (assoc (lisp-implementation-type) *init-filename* :test #'string-equal))
+   (user-homedir-pathname)))
+
+(defun write-detail (&key name key value)
+  "Write detail NAME with VALUE.
+Additionally, when running on GitHub Actions, the key is written
+to job output."
+  (format t "~&~A: ~A~%" name value)
+  (when (uiop:getenv "GITHUB_OUTPUT")
+    (with-open-file (output (uiop:getenv "GITHUB_OUTPUT")
+			    :direction :output
+			    :if-exists :append :if-does-not-exist :create)
+      (format output "~&~A=~A~%" key value))))
+
+(defun write-implementation-details ()
+  "Write details about the current Common Lisp Implementation."
+  (write-detail
+   :name "Lisp Implementation Type"
+   :key "lisp-implementation-type"
+   :value (lisp-implementation-type))
+  (write-detail
+   :name "Lisp Implementation Version"
+   :key "lisp-implementation-version"
+   :value (lisp-implementation-version))
+  (write-detail
+   :name "Lisp Implementation Init Filename"
+   :key "lisp-implementation-init-filename"
+   :value (init-filename)))
+
+(progn (write-implementation-details))
+
+;;;; End of file `describe-lisp.lisp'
+
